e8a882ac162993294e391bdaa4d70508071add69
mmierzva <mierzvamayara@gmail.com>
2025-09-16 14:01:50 -0300

    Update action_contato.php

--- action_contato.php
+++ action_contato.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guitar Shop - Resultado do Formulário</title>
+    <title>Guitar Shop - Formulário</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -23,7 +23,7 @@
 
     <!-- Conteúdo principal -->
     <main>
-        <h1>Dados Recebidos</h1>
+        <h1>Dados Enviados</h1>
         <div class="contato-form">
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
